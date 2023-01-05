306f6dd69b8f05f0ff2d0a4dc6c16f6efd5809b5
kieransjames <40867725+kieransjames@users.noreply.github.com>
2023-01-05 00:09:36 -0500

    Update 1_Team_Stats.py

--- rl_db_app/pages/1_Team_Stats.py
+++ rl_db_app/pages/1_Team_Stats.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+# configure page to wide layout, set title & icon
 st.set_page_config(layout="wide",
                    page_title="RL-DB: Team Stats",
                    page_icon=":video_game:",
@@ -8,13 +10,12 @@ st.set_page_config(layout="wide",
                     "About":None
                    })
 
-
+# remove border around submit container
 css = r'''
     <style>
         [data-testid="stForm"] {border: 0px}
     </style>
 '''
-
 st.markdown(css, unsafe_allow_html=True)
 
 
@@ -25,33 +26,42 @@ import mysql.connector
 from mysql.connector import Error
 import datetime
 import json
+from st_aggrid import AgGrid, GridOptionsBuilder
 
+
+# make sure we're in the right directory to find 'Event_dic.txt'
 import os 
 if "/rl_db_app/pages" not in os.getcwd():
     os.chdir('./rl_db_app/pages')
 
+# load json with dates for each event, in order to classify which split a game was played in, whether it's a regional or major, whether it's a qualifier or main event
 with open('Event_dic.txt') as f:
     data = f.read()
 event_dates = json.loads(data)
 
-
+# use json 'event_dates' to get split and event types
 def get_event_type(date_str,region,event_dic):
 
+#   check for invalid region
     if region not in ["APAC","EU","MENA","NA","OCE","SAM","SSA"]:
         return ("INVALID","INVALID","INVALID")
 
+#   narrow our search to the given region
     temp_dic = event_dic[region]
 
+# get year-month-date as a string
     date_str = date_str.split("T")[0]
-
+# convert to datetime
     date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
 
+#   assign split
     if date >= datetime.datetime(year=2022, month=9, day=1) and date <= datetime.datetime(year=2022, month=12, day=31):
         split="Fall"
     elif date >= datetime.datetime(year=2023, month=1, day=1) and date <= datetime.datetime(year=2023, month=4, day=15):
         split="Winter"
     elif date >= datetime.datetime(year=2023, month=4, day=16) and date <= datetime.datetime(year=2023, month=7, day=15):
         split="Spring"
+# check if we're actually at worlds 
     else:
         try:
             start_date = datetime.datetime.strptime(event_dic["Majors"]["Worlds"]["start"], '%Y-%m-%d')
@@ -64,9 +74,10 @@ def get_event_type(date_str,region,event_dic):
             pass
         
 
-
+# narrow our search down to which split we're in
     temp_dic = temp_dic[split]
 
+#  iterate through events in the split to find event
     for event in temp_dic:
         for sub_event in temp_dic[event]:
             try:
@@ -76,7 +87,8 @@ def get_event_type(date_str,region,event_dic):
                     return (split,"Regional",sub_event)
             except:
                 pass
-
+              
+# check the major separately, since majors don't technically have a region 
     for split in event_dic["Majors"]:
         try:
             start_date = datetime.datetime.strptime(event_dic["Majors"][split]["Main Event"]["start"], '%Y-%m-%d')
@@ -86,6 +98,7 @@ def get_event_type(date_str,region,event_dic):
         except:
             pass
            
+# if nothing worked, return invalid
     return ("INVALID","INVALID","INVALID")
 
 # load API token (which was not free)
@@ -95,8 +108,8 @@ db_address = json_credentials['db_address']
 db_name = json_credentials['db_name']
 password = json_credentials['password']
 
-# Connect to MySQL database
-
+# Connect to MySQL database, and load relevant info
+# cache results so the app doesn't bother the database for every user; instead, it will load once when the app is booted, and keep the results cached
 @st.cache(show_spinner=False)
 def init_app():
     try:
@@ -247,9 +260,11 @@ def init_app():
     except Error as e:
         print(e)
     	
+# contact the DB
 with st.spinner('Connecting to database, standby...'):
     dfs = init_app()
 
+# unpack our results from the DB
 team_meta_register = dfs['team_meta_register']
 team_base_stats = dfs['team_base_stats']
 team_boost_stats_for = dfs['team_boost_stats_for']
@@ -261,9 +276,10 @@ team_movement_stats_against = dfs['team_movement_stats_against']
 team_positioning_stats_against = dfs['team_positioning_stats_against']
 
 
-# list of teams
+# get list of teams
 teams = sorted(list(team_meta_register['Team Name'].unique()))
 
+# given a dataframe game by game stats, group stats by team (either summing or averaging)
 def aggregate_df(df,columns,per_game=True):
     temp1 = df.copy(deep=True)
     if per_game:
@@ -288,7 +304,8 @@ def aggregate_df(df,columns,per_game=True):
 
 
 
-
+# the 'default' table is what shows when the page initially loads, showing base stats for all teams
+# we cache this because why wouldn't we
 @st.cache(show_spinner=True)
 def default_table():
     display_df = team_meta_register.merge(team_base_stats,on=["Team Name","Game Id"],how="left").drop(["Game Id","opponent_name","color","opponent_color",
@@ -296,6 +313,7 @@ def default_table():
                                                                                                        "Event Type","Event Sub-type"],axis=1)
     return aggregate_df(display_df,list(display_df.columns),True)
 
+#  functions that round all numeric columns
 def custom_round_0(x):
     if pandas.api.types.is_numeric_dtype(x):
         return round(x)
@@ -313,17 +331,22 @@ def custom_round_1(x):
 st.title('Team Stats')
 st.subheader("Below are the up-to-date cumulative statistics for the RLCS 2022-2023 season, by team. Submit a query using the filters below for more specific stats.")
 
+# define four header columns
 col1_header, col2_header, col3_header, col4_header = st.columns(4)
 
+# an option to filter by date
 with col4_header:
     date_check = st.checkbox('Filter by date')
 
+# load the default table    
 default_df = default_table()
 
+# bundle our query options together as a form 
 with st.form("my_form"):
     st.write("Select parameters:")
     col1, col2, col3, col4 = st.columns(4)
 
+# filter by split and event types
     with col1:
         Split = st.multiselect(
             'Split',
@@ -343,7 +366,7 @@ with st.form("my_form"):
             []
         )
 
-
+# filter by team and region
     with col2:
         teams_for = st.multiselect(
             'Team',
@@ -355,7 +378,7 @@ with st.form("my_form"):
             ["APAC","EU","MENA","NA","OCE","SAM","SSA"],
             []
         )
-
+# choose to sum or average game stats
         per_game = st.radio(
         "hidden",
         ('Per game stats', 'Total stats'),
@@ -366,6 +389,7 @@ with st.form("my_form"):
         else:
             per_game_bool = False
 
+# filter by opponent and opponent region
     with col3:
         teams_against = st.multiselect(
             'Opposing team',
@@ -378,7 +402,7 @@ with st.form("my_form"):
             []
         )
 
-
+# optionally filter by date 
     with col4:
         if date_check:
             start_date = st.date_input(
@@ -390,22 +414,27 @@ with st.form("my_form"):
                 datetime.date(2023,1,1)
             )
 
-
+# choose which statistical categories the user wants to see
+# I don't want to present all of them at once to avoid crowding the table
     Stats_select = st.multiselect(
             'Statistical categories:',
             ["Base stats","Demos","Boost stats","Boost stats (Against)","Movement","Movement (Against)","Positioning","Positioning (Against)"],
             []
         )
 
+# get all games  
     temp_df = team_meta_register
 
+# get upper bound of games played
     max_GP = int(default_df['GP'].max())
 
+#   filter by games played
     min_gp = st.slider(
         "Minimum Games Played:",
         0,max_GP, (0,max_GP)
         )
 
+#   once query is submitted, filter temp_df
     submitted = st.form_submit_button("Submit query")
     if submitted:
     
@@ -455,6 +484,7 @@ with st.form("my_form"):
 
 
 # merge with requested stat categories
+# if no categories chosen, just return base stats
         if len(Stats_select)==0:
             temp_df = temp_df.merge(team_base_stats,on=["Team Name","Game Id"],how="left").drop(["Game Id",
                                                                                                 "opponent_name",
@@ -464,37 +494,43 @@ with st.form("my_form"):
                                                                                                 "opponent_region",
                                                                                                 "Split","Event Type","Event Sub-type"],axis=1)
         else:
+# iterate through chosen categories and merge           
             for stats_cat in Stats_select:
                 temp_df = temp_df.merge(stats_cats_dic[stats_cat],on=["Team Name","Game Id"],how="left")
             temp_df = temp_df.drop(["Game Id","opponent_name","color","opponent_color","game_date","opponent_region","Split","Event Type","Event Sub-type"],axis=1)
 
-        
+# aggregate stats
         display_df = aggregate_df(temp_df,list(temp_df.columns),per_game_bool)
 
+# for per game stats, we round to one digit   
         if per_game_bool:
             display_df = display_df.apply(custom_round_1)
         else:
+# for total stats, we take ints           
             display_df = display_df.apply(custom_round_0)
-        
+# filter by games played         
         display_df = display_df[(display_df['GP']>=min_gp[0]) & (display_df['GP']<=min_gp[1])]
 
+# if the form hasn't been submitted, display the default table
     if not submitted:
         display_df = default_df
         display_df = display_df.apply(custom_round_1)
 
 
-
-
-from st_aggrid import AgGrid, GridOptionsBuilder
+# use 'aggrid' to display the table
 gb = GridOptionsBuilder.from_dataframe(display_df)
 
+# define minimum column width
 base_width = 80
+# scale column width with length of column label
 width_factor = 5
 
+# set column configurations
 for col in list(display_df.columns):
 
     col_len = len(col)
 
+# pin 'Team Name' to the left of the table     
     if col == "Team Name":
         gb.configure_column(col, precision=1, width=190,pinned="left")
     elif col == "GP":
@@ -503,5 +539,5 @@ for col in list(display_df.columns):
         gb.configure_column(col, precision=1, width=(col_len*width_factor+base_width))
 
 
-
+# display table, specify "NO_UPDATE" so it doesn't reload to the default every time the user sorts one of the columns
 AgGrid(display_df, gridOptions=gb.build(),height=800,update_mode="NO_UPDATE")
